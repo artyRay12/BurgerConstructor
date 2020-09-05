16f6dc9296029a5e8249548041528d6b289b2500
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-09-05 08:30:44 +0300

    Updates

---
+++ precache-manifest.9461152368f38fc58aa698f7ebf5bb9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3165b31ea4f5b32a6ab8662a9a47ca66",
+    "url": "/BurgerConstructor/index.html"
+  },
+  {
+    "revision": "d463fc1fa4b3e83096bf",
+    "url": "/BurgerConstructor/static/css/main.956a4130.chunk.css"
+  },
+  {
+    "revision": "9406d653428ae807f50c",
+    "url": "/BurgerConstructor/static/js/2.36c2a215.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerConstructor/static/js/2.36c2a215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d463fc1fa4b3e83096bf",
+    "url": "/BurgerConstructor/static/js/main.9ae84bae.chunk.js"
+  },
+  {
+    "revision": "3912638f2cf090b241d4",
+    "url": "/BurgerConstructor/static/js/runtime-main.5b2d20c4.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerConstructor/static/media/burger-logo.b8503d26.png"
+  }
+]);
